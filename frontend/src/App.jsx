361c211bf1fa25ee9438b4acd38024b903bb90c3
Khushiipd <Khushiprasad2310@gmail.com>
2026-07-11 11:54:37 +0530

    Initialize React frontend and create home page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "80px" }}>
+      <h1>AI Research Funding Platform</h1>
+      <h2>Infosys Springboard Internship Project</h2>
+
+      <br />
+
+      <button>Login</button>
+
+      <button style={{ marginLeft: "20px" }}>
+        Register
+      </button>
+
+      <br /><br />
+
+      <p>
+        Empowering Researchers with AI-driven Funding Discovery
+      </p>
+    </div>
+  );
+}
+
+export default App;
